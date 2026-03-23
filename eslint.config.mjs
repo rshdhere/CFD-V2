cf01bd293fec39bb4dfb2b28244a6bc9e043065e
raashed <mohammedraashed10@gmail.com>
2026-03-23 14:27:20 +0530

    feat(turbo): updated naming from repo to CFD-V2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,5 @@
-import { config as baseConfig } from "@repo/eslint-config/base";
-import { config as reactInternalConfig } from "@repo/eslint-config/react-internal";
-import { nextJsConfig } from "@repo/eslint-config/next-js";
+import { config as baseConfig } from "@CFD-V2/eslint-config/base";
+import { config as reactInternalConfig } from "@CFD-V2/eslint-config/react-internal";
+import { nextJsConfig } from "@CFD-V2/eslint-config/next-js";
 
 export default [...baseConfig, ...reactInternalConfig, ...nextJsConfig];
