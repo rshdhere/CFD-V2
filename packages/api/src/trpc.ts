dd96503ef4b6eca9b19a0690e393abda24b35ba5
raashed <mohammedraashed10@gmail.com>
2026-03-23 15:02:50 +0530

    feat(tRPC): added trpc-server init-file

---
+++ packages/api/src/trpc.ts
@@ -0,0 +1,25 @@
+import { Response } from "express";
+import { initTRPC, TRPCError } from "@trpc/server";
+
+export type Context = {
+  userId?: string;
+  res: Response;
+};
+
+const t = initTRPC.context<Context>().create();
+
+export const router = t.router;
+export const publicProcedure = t.procedure;
+export const privateProcedure = t.procedure.use(({ ctx, next }) => {
+  if (!ctx.userId) {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+    });
+  }
+
+  return next({
+    ctx: {
+      userId: ctx.userId,
+    },
+  });
+});
