b3ac79cb0603673264860f15cba7a6a8b3b3894a
raashed <mohammedraashed10@gmail.com>
2026-03-25 19:04:04 +0530

    fix(lint): fixed linting-issues from /drizzle package

--- packages/drizzle/src/index.ts
+++ packages/drizzle/src/index.ts
@@ -2,5 +2,4 @@ import "dotenv/config";
 export { and, eq } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/node-postgres";
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const db = drizzle(process.env.DATABASE_URL!);
+export const db = drizzle(process.env.DATABASE_URL!);
