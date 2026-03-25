974872e0f9425f2e00a5920d11847fe5af2b86b5
raashed <mohammedraashed10@gmail.com>
2026-03-25 13:45:24 +0530

    feat(types): added types for all auth-logic

--- packages/validators/src/index.ts
+++ packages/validators/src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import { z } from "zod";
+import { authSchema } from "./user/validation.js";
+
+export * from "./user/validation.js";
+
+export type AuthSchema = z.infer<typeof authSchema.input>;
+export type SignUpSchema = AuthSchema;
+export type SignInSchema = AuthSchema;
+export type ResendVerificationSchema = z.infer<
+  typeof authSchema.resendVerificationInput
+>;
