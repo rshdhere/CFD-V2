ba088c2f952556b2c09bfa06ec0d04388ed03ba1
raashed <mohammedraashed10@gmail.com>
2026-03-25 13:44:45 +0530

    feat(authSchema): added auth-schema for /signup and /login tRPC's procedures

---
+++ packages/validators/src/user/validation.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+
+export const authSchema = {
+  input: z
+    .object({
+      email: z.email({ message: "invalid email for sign-up procedure" }),
+      password: z
+        .string()
+        .min(8, { message: "password should be minimum of 8 charachters" })
+        .max(24, { message: "password should be maximum of 24 charachters" })
+        .regex(
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).{8,}$/,
+          {
+            message:
+              "password must contain at least one upper-case letter, one lower-case letter, a number, and a special-character",
+          },
+        ),
+    })
+    .strict(),
+
+  resendVerificationInput: z
+    .object({
+      email: z.email({ message: "invalid email for verification" }),
+    })
+    .strict(),
+
+  output: z
+    .object({
+      accessToken: z.jwt(),
+      message: z.string(),
+    })
+    .strict(),
+};
