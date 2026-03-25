36e08436e094731bd5ccdd1278d2601fef980fa3
raashed <mohammedraashed10@gmail.com>
2026-03-25 19:11:57 +0530

    chore(email): importing email-template using the fs-library

--- packages/services/src/index.ts
+++ packages/services/src/index.ts
@@ -1,1 +1,6 @@
-console.log("Hello via Bun!");
+import { readFile } from "node:fs/promises";
+
+export const verificationEmailTemplatePromise = readFile(
+  new URL("./email/email.html", import.meta.url),
+  "utf8",
+);
