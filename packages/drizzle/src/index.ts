4865750243f0c4ccdde2ef5b16113ed538d723c6
raashed <mohammedraashed10@gmail.com>
2026-03-25 13:49:05 +0530

    refactor(drizzle): exporting 'and, eq' to the /turborepo

--- packages/drizzle/src/index.ts
+++ packages/drizzle/src/index.ts
@@ -1,4 +1,6 @@
 import "dotenv/config";
+export { and, eq } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/node-postgres";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const db = drizzle(process.env.DATABASE_URL!);
