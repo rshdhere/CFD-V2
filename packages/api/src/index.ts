8e6a565fb1f8f34940bdc5bb9b0c1f99ef24969e
raashed <mohammedraashed10@gmail.com>
2026-03-25 19:24:03 +0530

    feat(express): added express-adapter from tRPC & handlers to managing /api

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,1 +1,31 @@
-console.log("Hello via Bun!");
+import cors from "cors";
+import express from "express";
+import { appRouter } from "./router.js";
+import cookieParser from "cookie-parser";
+import { CLIENT_URL } from "@CFD-V2/config";
+import { createContext } from "./middleware/context.js";
+import { refreshHandler } from "./handlers/refresh-handler.js";
+import { logoutHandler } from "./handlers/logout-handler.js";
+import * as trpcExpress from "@trpc/server/adapters/express";
+import { handleEmailVerification } from "./handlers/email-handler.js";
+
+export const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+    origin: CLIENT_URL,
+  }),
+);
+
+app.use(cookieParser());
+app.get("/trpc/verify-email", handleEmailVerification);
+app.use(
+  "/trpc",
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext,
+  }),
+);
+app.post("/refresh", refreshHandler);
+app.post("/refresh/logout", logoutHandler);
