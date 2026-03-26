e52a2b5137c78995f142506434cdb920c38a21ae
raashed <mohammedraashed10@gmail.com>
2026-03-26 13:43:45 +0530

    feat(server): initialized /server to run on a server port while ensuring database-connection

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import { app } from "@CFD-V2/api";
+import { runTime, SERVER_PORT } from "@CFD-V2/config";
+import { ensureDatabaseConnection } from "@CFD-V2/drizzle/health";
+
+export const main = async () => {
+  await ensureDatabaseConnection();
+
+  app.listen(SERVER_PORT, () => {
+    console.log(
+      `server is running in ${runTime}-mode on http://localhost:${SERVER_PORT}`,
+    );
+  });
+};
