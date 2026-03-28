53901b7b88da4cf04797cd712ab67ffba8cbe4e8
raashed <mohammedraashed10@gmail.com>
2026-03-28 17:58:38 +0530

    feat(seed): added seed script for /database

--- packages/drizzle/src/index.ts
+++ packages/drizzle/src/index.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-export { and, eq } from "drizzle-orm";
+export { and, eq, sql } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/node-postgres";
 
 export const db = drizzle(process.env.DATABASE_URL!);
