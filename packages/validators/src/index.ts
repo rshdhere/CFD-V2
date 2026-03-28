40e59d8a8769add451b3286de1ef74985e058fc1
raashed <mohammedraashed10@gmail.com>
2026-03-28 17:42:18 +0530

    feat(types): added types for CandleQuery

--- packages/validators/src/index.ts
+++ packages/validators/src/index.ts
@@ -1,9 +1,11 @@
 import { z } from "zod";
 import { authSchema } from "./user/validation.js";
+import { candlesSchema } from "./candles/validation.js";
 
 export * from "./user/validation.js";
 
 export type AuthSchema = z.infer<typeof authSchema.input>;
+export type CandleQuery = z.infer<typeof candlesSchema.getAllInput>;
 export type SignUpSchema = AuthSchema;
 export type SignInSchema = AuthSchema;
 export type ResendVerificationSchema = z.infer<
