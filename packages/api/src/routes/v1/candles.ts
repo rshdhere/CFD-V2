3e70f4135723cd2fcb6830ad63551adab8d6383e
raashed <mohammedraashed10@gmail.com>
2026-03-28 18:14:26 +0530

    feat(candles): added /candles as a tRPC-procedure

--- packages/api/src/routes/v1/candles.ts
+++ packages/api/src/routes/v1/candles.ts
@@ -1,3 +1,25 @@
-import { router } from "@/src/trpc.js";
+import { TRPCError } from "@trpc/server";
+import { fetchBinanceKlines } from "@CFD-V2/services/market";
+import { candlesSchema } from "@CFD-V2/validators/candles";
+import { privateProcedure, router } from "@/src/trpc.js";
 
-export const candleRouter = router({});
+export const candleRouter = router({
+  getAll: privateProcedure
+    .input(candlesSchema.getAllInput)
+    .output(candlesSchema.getAllOutput)
+    .query(async ({ input }) => {
+      try {
+        const candles = await fetchBinanceKlines(input);
+        return { candles };
+      } catch (e) {
+        if (e instanceof TRPCError) {
+          throw e;
+        }
+        console.error("failed to fetch candles", e);
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "failed to fetch candles",
+        });
+      }
+    }),
+});
