86ff0bfb90a4520d0685e83642f1bf49a747268b
raashed <mohammedraashed10@gmail.com>
2026-03-29 10:42:59 +0530

    feat(auth): added /auth's layout-page

---
+++ apps/web/app/(auth)/layout.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import React from "react";
+import { useRequireGuest } from "@/utils/auth/guards";
+import { FastLoader } from "@/components/loader";
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { isAuthReady, isAuthenticated } = useRequireGuest();
+
+  if (!isAuthReady || isAuthenticated) {
+    return (
+      <div className="flex min-h-screen items-center justify-center">
+        <FastLoader content="Loading" />
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      {children}
+    </div>
+  );
+}
